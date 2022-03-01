8dd476f36566ca8a40c9466a24668727c3aaadde
kts <kts168@163.com>
2022-03-01 21:39:35 +0800

    one

---
+++ 1.下拉.bat
@@ -0,0 +1,3 @@
+cd /d %~dp0
+git pull git@github.com:kts168/mydocker.git
+::git pull origin master --allow-unrelated-histories
---
+++ 2.git.push推送.bat
@@ -0,0 +1,12 @@
+@echo off
+setlocal enabledelayedexpansion
+set fixed=
+set /p "fixed=请输入本次提交修改点: "
+cd /d %~dp0
+::echo %fixed%
+::pause
+
+@echo on
+git add .
+git commit -m %fixed%
+git push --set-upstream git@github.com:kts168/mydocker.git master
